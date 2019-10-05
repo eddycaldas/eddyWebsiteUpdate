03af7bd69b3c9b2d877c80240c349d071a33e1d8
Eddy Caldas <eddycaldas@live.com>
2019-10-05 10:41:00 -0600

    add neomi website

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('works')
+
+
+const time = function = () => {
+    const date = new Date()
+    const year = date.getFullYear()
+    return `<li>&copy; Eddy Caldas. All rights reserved.</li><li>${year}</li>`
+}
+document.getElementById('copyright', 'time')
